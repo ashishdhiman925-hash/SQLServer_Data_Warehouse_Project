ef69250dcad8fafc468e2d649c6eee2fc2a74115
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-09-17 22:37:02 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,6 @@
+--- This scripts check if we already have database datawarehouse or not, if not, then it will create it, and if yes then it will drop it to create one.
+---- This scripts then create the schema under the datawarehouse table : schema is like a folder where you will manage everything. 
+
 -- check first if database exists or not 
 if exists (select 1 from sys.databases where name = 'Datawarehouse')
 begin

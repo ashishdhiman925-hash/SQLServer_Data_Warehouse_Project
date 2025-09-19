dad067dc8f361f81865586a3f6faff26c9907cbf
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-09-19 13:15:43 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -82,13 +82,73 @@ create table bronze.erp_px_cat_g1v2 (
 	subcat nvarchar (50),
 	maintenance nvarchar(50)
 );
---- first bulk load of cust_info
-bulk insert bronze.crm_cust_info
-From 'C:\Users\Deepika\Desktop\cust_info.csv'
-with (
-	Firstrow = 2,
-	fieldterminator = ',',
-	tablock
-);
+--- first bulk load of data into Store Prodecure , this SP name is bronze.load_brone, you can exe it by exec bronze.load_brone
+
+Create or alter procedure bronze.load_bronze as
+Begin
+
+	truncate table bronze.crm_cust_info;
+
+	bulk insert bronze.crm_cust_info
+	from 'C:\Users\Deepika\Desktop\cust_info.csv'
+	with (
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
+
+	truncate table bronze.crm_prd_info;
+
+	bulk insert bronze.crm_prd_info
+	from 'C:\Users\Deepika\Desktop\prd_info.csv'
+	with (
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
+
+	truncate table bronze.crm_sales_details;
+
+	bulk insert bronze.crm_sales_details
+	from 'C:\Users\Deepika\Desktop\sales_details.csv'
+	with (
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
+
+	--- load erp table
+
+	truncate table bronze.erp_cust_az12;
+
+	bulk insert bronze.erp_cust_az12
+	from 'C:\Users\Deepika\Desktop\CUST_AZ12.csv'
+	with (
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
+
+	truncate table bronze.erp_loc_a101;
+
+	bulk insert bronze.erp_loc_a101
+	from 'C:\Users\Deepika\Desktop\LOC_A101.csv'
+	with (
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
+
+	truncate table bronze.erp_px_cat_g1v2;
+
+	bulk insert bronze.erp_px_cat_g1v2
+	from 'C:\Users\Deepika\Desktop\PX_CAT_G1V2.csv'
+	with (
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock
+	);
+END
+
 
 

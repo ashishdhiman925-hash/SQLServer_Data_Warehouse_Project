4cdf9d35b057fb821d7b6cec3f6a45c19029d6e7
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-09-20 12:22:35 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -238,3 +238,38 @@ create table silver.erp_px_cat_g1v2 (
 	maintenance nvarchar(50),
 	dwh_create_date datetime2 default getdate()	
 );
+
+
+------ Data Cleaning, duplicates and loading to silver layer Script.
+insert into silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_material_status,
+	cst_gndr,
+	cst_create_date
+)
+select
+
+	cst_id,
+	cst_key,
+	trim(cst_firstname) as cst_firstname,
+	trim(cst_lastname) as cst_lastname,
+	case when upper(trim(cst_material_status)) = 'M' then 'Married'
+		 when upper(trim(cst_material_status)) = 'S' then 'Single'
+		 else 'n/a'
+	End cst_material_status,
+	case when upper(trim(cst_gndr)) = 'F' then 'Female'
+		 when upper(trim(cst_gndr)) = 'M' then 'Male'
+		 else 'n/a'
+	End cst_gndr,
+	cst_create_date
+
+from (
+	select 
+	*,
+	row_number() over (partition by cst_id order by cst_create_date DESC) as flag_last
+	from bronze.crm_cust_info
+	--where cst_id = 29466
+	) t where flag_last = 1

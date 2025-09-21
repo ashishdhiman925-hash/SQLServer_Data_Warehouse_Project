8602524e7bd288330d3c222f237e176656d32724
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-09-21 15:06:38 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -314,3 +314,43 @@ from bronze.crm_prd_info
 --where replace(SUBSTRING(prd_key,1, 5),'-','_') not in	
 --(select distinct id from bronze.erp_px_cat_g1v2)
 --select sls_prd_key from bronze.crm_sales_details
+
+
+------ Silver load script 3 - Data Cleaning, duplicates and loading to silver layer Script.
+insert into silver.crm_sales_details (
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+)
+Select 
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    case when sls_order_dt = 0 or len(sls_order_dt) != 8 then null
+         else cast(cast(sls_order_dt as varchar) as date)
+    end as sls_order_dt,
+    case when  sls_ship_dt = 0 or len(sls_ship_dt) != 8 then null
+         else cast(cast(sls_ship_dt as varchar) as date)
+    end as  sls_ship_dt,
+    case when  sls_due_dt = 0 or len(sls_due_dt) != 8 then null
+         else cast(cast(sls_due_dt as varchar) as date)
+    end as  sls_due_dt,
+    case when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price)
+	     then sls_quantity * abs(sls_price)
+        else sls_sales
+    end as sls_sales,
+    sls_quantity,
+    case when sls_price is null or sls_price <= 0 
+	    then sls_sales / nullif(sls_quantity,0)
+        else sls_price
+    end as sls_price
+from bronze.crm_sales_details
+--where sls_ord_num != trim(sls_ord_num)
+--where sls_cust_id not in (select cst_id from silver.crm_cust_info)
+--select * from silver.crm_sales_details

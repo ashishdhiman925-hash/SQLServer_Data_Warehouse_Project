8d7dbbd3d76194f4548b8184d436119ff9119494
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-09-22 09:11:24 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -814,3 +814,23 @@ on pn.cat_id = pc.id
 where prd_end_dt is null -- filter out all historical data
 --)t group by prd_key
 --having count(*) > 1
+
+
+----------------------------Gold View 3 : Fact Table - Joining Dimensions Sarrogate Keys ------------------------
+
+Create View gold.fact_sales as 
+select 
+	sd.sls_ord_num as order_number,
+	pr.product_key,
+	cu.customer_key,
+	sd.sls_order_dt as order_date,
+	sd.sls_ship_dt as shipping_date,
+	sd.sls_due_dt as due_date,
+	sd.sls_sales as sales_amount,
+	sd.sls_quantity as quantity,
+	sd.sls_price price
+From silver.crm_sales_details sd
+left join gold.dim_products pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers cu
+on sd.sls_cust_id = cu.customer_id

3f01cc44e6b2e44217e4cdbf9c91776ae405fd99
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-09-22 08:43:46 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -790,3 +790,27 @@ END
 --)t group by cst_id
 --having count(*) > 1
 --select * from gold.dim_customers
+
+
+------------------------- Gold View 2 : Build Dimension Products ----------------------------------View 
+
+--Select prd_key, count(*) from (
+create view gold.dim_products as 
+Select
+	ROW_NUMBER () over (order by pn.prd_start_dt, pn.prd_key) as product_key,
+	pn.prd_id as product_id,
+	pn.prd_key as product_number,
+	pn.prd_nm as product_name,
+	pn.cat_id as category_id,
+	pc.cat as category ,
+	pc.subcat as subcategory,
+	pc.maintenance,
+	pn.prd_cost as cost,
+	pn.prd_line as product_line,
+	pn.prd_start_dt as start_date
+From silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is null -- filter out all historical data
+--)t group by prd_key
+--having count(*) > 1

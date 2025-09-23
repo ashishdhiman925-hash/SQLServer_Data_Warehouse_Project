238ca1bfc6dd632fe142309d01aa5bbefde412c0
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-09-23 11:21:33 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -895,3 +895,41 @@ from
 	group by datetrunc(YEAR,order_date)
 	--order by datetrunc(month,order_date)
 )t
+
+------------------------------------ Year over Year Using CTE ---------------------------
+--analyze the yearly performance of products by comparing  their sales to both the average sales performance of the product
+-- and the previous year sales. 
+
+with yearly_product_sales as 
+	(
+	Select
+		year (f.order_date) as order_year,
+		p.product_name,
+		sum(f.sales_amount) as current_sales
+	from gold.fact_sales f
+	left join gold.dim_products p
+	on f.product_key = p.product_key
+	where order_date is not null 
+	group by
+	year(f.order_date),p.product_name
+	)
+
+select
+	order_year,
+	product_name,
+	current_sales,
+	avg(current_sales) over (partition by product_name) avg_sales,
+	current_sales - avg(current_sales) over (partition by product_name) as diff_avg,
+	case when current_sales - avg(current_sales) over (partition by product_name) > 0 then 'Above Average'
+		 when current_sales - avg(current_sales) over (partition by product_name) < 0 then 'Below Average'
+		 else 'Avg'
+	end avg_change,
+	----- year over year analysis----------------------------
+	lag(current_sales) over (partition by product_name order by order_year) py_sales,
+	current_sales - lag(current_sales) over (partition by product_name order by order_year) as diff_py,
+	case when current_sales - lag(current_sales) over (partition by product_name order by order_year) > 0 then 'Increase'
+		 when current_sales - lag(current_sales) over (partition by product_name order by order_year) < 0 then 'Decrease'
+		 else 'No Change'
+	end py_change
+from yearly_product_sales
+order by product_name,order_year

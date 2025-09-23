258d9fe89343af19a709f31ccede9a99656a1f66
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-09-23 11:35:24 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -933,3 +933,26 @@ select
 	end py_change
 from yearly_product_sales
 order by product_name,order_year
+
+
+----------------------------------------- Part to whole analysis---------------------------
+---- part to whole analysis 
+---- categories contribute most to the overall sales
+
+with category_sales as
+	(
+	Select 
+		category,
+		sum(sales_amount) total_sales	
+	from gold.fact_sales f
+	left join gold.dim_products p
+	on p.product_key = f.product_key
+	group by category
+	)
+Select
+	category,
+	total_sales,
+	sum(total_sales) over () overall_sales,
+	concat(round((cast(total_sales as float)/sum(total_sales) over()) *100,2), '%') as percentage_of_total
+from category_sales
+order by total_sales DESC

47b12c5ceebe29e654e48c1580a2a6623ae71b11
ashishdhiman925-hash <ashishdhiman925@gmail.com>
2025-09-23 14:04:18 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -979,3 +979,30 @@ count(product_key) as total_products
 from Product_Segment
 group by cost_range
 Order by total_products DESC
+------------------------------------------ Final Aggregation ----------------------------------------------------------
+with customer_spending as
+(
+Select 
+c.customer_key,
+sum(f.sales_amount) as total_spending,
+min(order_date) as fist_order,
+max(order_date) as last_order,
+datediff(month,min(order_date), max(order_date)) as lifespan
+from gold.fact_sales f
+left join gold.dim_customers c
+on f.customer_key = c.customer_key
+group by c.customer_key
+)
+select 
+customer_segment,
+count(customer_key) as total_customer
+from(
+	select 
+	case when lifespan > 12 and total_spending > 5000 then 'VIP'
+		 when lifespan > 12 and total_spending < 5000 then 'Regular'
+		 Else 'New'
+	End customer_segment
+	from customer_spending
+)t
+group by customer_segment
+order by total_customer DESC
